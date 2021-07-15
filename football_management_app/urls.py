d430aa32ea14d70921e99edb1aa8fa22b7706f4c
kzah74 <krista.zaharieva.r@gmail.com>
2021-07-15 18:41:42 +0300

    styling

--- football_management_app/urls.py
+++ football_management_app/urls.py
@@ -1,7 +1,8 @@
 "Defines URL patterns for football_management_app."
 
 from django.urls import path
-from .views import TeamListView, TeamCreateView, GenerateMatchesView, MatchScheduleListView, MatchesListView, TeamUpdateView, TeamDeleteView, PlayerCreateView, PlayersListView
+from .views import TeamListView, TeamCreateView, GenerateMatchesView, MatchScheduleListView, MatchesListView, TeamUpdateView, TeamDeleteView
+from .views import PlayerCreateView, PlayersListView, PlayerUpdateView, PlayerDeleteView
 from django.views.generic import TemplateView
 from django.contrib.admin.views.decorators import staff_member_required
 
@@ -15,7 +16,7 @@ urlpatterns = [
     path('team/add/', TeamCreateView.as_view(), name='new_team'),
     # Page for generate button.
     path('generate_matches/', staff_member_required(GenerateMatchesView.as_view()), name='generate_matches'),
-    # Page for generating the matches.
+    # Page for matche schedule.
     path('match_schedule/', MatchScheduleListView.as_view(), name='match_schedule'),
     # Page that shows all matches.
     path('match_list/', MatchesListView.as_view(), name='match_list'),
@@ -26,5 +27,9 @@ urlpatterns = [
     # Creating a player.
     path('player/add/', PlayerCreateView.as_view(), name='new_player'),
     # Page with information about the players.
-    path('players/', PlayersListView.as_view(), name='players_list')
+    path('players/', PlayersListView.as_view(), name='players_list'),
+    # Updating player page.
+    path('player/<int:pk>/', staff_member_required(PlayerUpdateView.as_view()), name='player_update'),
+    # Delete Player.
+    path('player/<int:pk>/delete/', staff_member_required(PlayerDeleteView.as_view()), name='player_delete')
 ]
